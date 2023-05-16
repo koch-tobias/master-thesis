d7b31fef9d20fa6cfca74812f75661c6493d9501
koch-tobias <tobi.ko53@googlemail.com>
2023-05-16 23:09:01 +0200

    use model for prediction and display result

--- src/cpcs.py
+++ src/cpcs.py
@@ -4,11 +4,20 @@ import numpy as np
 import os 
 from datetime import datetime
 from shutil import copy2
+from pathlib import Path
+from loguru import logger
+import pickle
+import numpy as np
+from nltk.tokenize import WhitespaceTokenizer
+from ipynb.fs.defs.Feature_Engineering import add_new_features
+from ipynb.fs.defs.Feature_Engineering import preprocess_dataset
+from ipynb.fs.full.Prepare_data import prepare_and_add_labels
+from ipynb.fs.full.Prepare_data import prepare_text
 
 # create sidebar to upload the csv file and display the possible labels
 st.set_page_config(page_title="Car Part Classification", page_icon="../plots_images/logos/BMWGrey.svg")
 
-st.image("../plots_images/logos/BMW_Group_Grey.svg", use_column_width=False, width=5, output_format="SVG")
+#st.image("../plots_images/logos/BMW_Group_Grey.svg", use_column_width=False, width=5, output_format="SVG")
 
 st.title("Car Part Classification")
 col1, col2 = st.columns(2)
@@ -31,10 +40,59 @@ st.sidebar.markdown(
     unsafe_allow_html=True
 )
 
+dataframes = []
+
 # Display the uploaded file as a pandas dataframe
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
+    dataframes.append(df)
+    df, ncars = prepare_and_add_labels(dataframes, save_as_excel=False)
+        
+    # Load model
+    model_path = "../models/lgbm_16052023_1729/model_9552238805970149.pkl"
+    with open(model_path, "rb") as fid:
+        lgbm = pickle.load(fid)
+
+    # Load the vectorizer from the file
+    vectorizer_path = "../models/lgbm_16052023_1729/vectorizer.pkl"
+    with open(vectorizer_path, 'rb') as f:
+        vectorizer = pickle.load(f)
+
+    # Get the vocabulary of the training data
+    vocab_path = '../models/lgbm_16052023_1729/vocabulary.pkl'
+    with open(vocab_path, 'rb') as f:
+        vocabulary = pickle.load(f)
+
+    for i in range(len(df)):
+
+        df_preprocessed, df_for_plot = preprocess_dataset(df[i], cut_percent_of_front=0.25)
+
+        df_preprocessed["Benennung (dt)"] = df_preprocessed.apply(lambda x: prepare_text(x["Benennung (dt)"]), axis=1)
+
+        # Convert the vocabulary list to a dictionary
+        vocabulary_dict = {word: index for index, word in enumerate(vocabulary)}
+
+        # Set the vocabulary of the vectorizer to the loaded vocabulary
+        vectorizer.vocabulary_ = vocabulary_dict
+        X = vectorizer.transform(df_preprocessed['Benennung (dt)']).toarray()
+
+        # Combine text features with other features
+        #X = np.concatenate((X, df_preprocessed[['center_x', 'center_y', 'center_z','length','width','height','theta_x','theta_y','theta_z']].values), axis=1)
+
+        y_pred = lgbm.predict(X)
+        y_pred = np.round(y_pred)
+
+        for index, row in df_preprocessed.iterrows():
+            if y_pred[index] == 1: 
+                df_preprocessed.loc[index,'Relevant fuer Messung'] = 'Ja'
+            else:
+                df_preprocessed.loc[index,'Relevant fuer Messung'] = 'Nein'
+
+        df_preprocessed = df_preprocessed[df_preprocessed['Relevant fuer Messung'] == 'Ja']
+
+        df_preprocessed = df_preprocessed.loc[:,["Sachnummer", "Benennung (dt)", "Relevant fuer Messung", "Einheitsname"]]
+
     st.write("## Uploaded file:")
-    st.write(df)
+    st.write(df_preprocessed)
 else:
     st.write("## No file uploaded yet.")
