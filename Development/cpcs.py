8a67b42bddd28f2630c7d4090f9b3fa7a58de044
koch-tobias <tobi.ko53@googlemail.com>
2023-05-17 09:32:34 +0200

    change text in dashboard

--- Development/cpcs.py
+++ Development/cpcs.py
@@ -36,8 +36,7 @@ st.set_page_config(page_title="Car Part Classification", page_icon="plots_images
 
 st.title("Car Part Classification")
 col1, col2 = st.columns(2)
-st.sidebar.write("## Excel-Datei hochladen")
-uploaded_file = st.sidebar.file_uploader("# Excel-Datei hochladen", type="xls")
+uploaded_file = st.sidebar.file_uploader("# Upload Excel-file", type="xls")
 st.sidebar.image("plots_images/logos/BMWGrey.svg", use_column_width=True)
 
 
