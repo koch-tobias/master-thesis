0131a4592f6638d377e850d45508d52193110d6f
koch-tobias <tobi.ko53@googlemail.com>
2023-05-24 17:25:07 +0200

    change calues

--- Development/config.py
+++ Development/config.py
@@ -1,6 +1,6 @@
 lgbm_params = {
     "seed": 42,
-    "cut_percent_of_front": 0.25,
+    "cut_percent_of_front": 0.20,
     "test_size": 0.4,
     "metrics": ['auc', 'binary_logloss'],
      "n_estimators": 1000,
@@ -8,7 +8,7 @@ lgbm_params = {
 }
 
 lgbm_hyperparameter = {
-    "lr": [0.01],    
+    "lr": [0.1],    
     "max_depth": [7],
     "num_leaves": [128],
     "feature_fraction": [0.9],
