b838ba5e483c0603d99eede12e3ee6af40381b0f
koch-tobias <tobi.ko53@googlemail.com>
2023-06-13 14:26:49 +0200

    change layout of columns

--- Development/cpcs.py
+++ Development/cpcs.py
@@ -196,10 +196,10 @@ if authentication_status:
                                         data=df_xlsx ,
                                         file_name= f'{ncars[0]}_relevant_car_parts.xlsx')
         
-        col1, col2, col3 = st.columns(3)
-        with col2:
-            if len(einheitsname_not_found) > 0:
-                st.write("The following parts are not found in the uploaded data: ")
+        col1, col2, col3 = st.columns([1, 5, 1])
+        if len(einheitsname_not_found) > 0:
+            st.write("The following parts are not found in the uploaded data: ")
+            with col2: 
                 st.write("\t" + "- " + "\n- ".join(einheitsname_not_found))
 
         st.subheader("Feedback Email Template")
