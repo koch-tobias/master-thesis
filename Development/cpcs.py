e4005a2a488214ede6bbef65e6806ae561288f72
koch-tobias <tobi.ko53@googlemail.com>
2023-06-13 14:40:22 +0200

    center title

--- Development/cpcs.py
+++ Development/cpcs.py
@@ -93,7 +93,7 @@ def hide_streamlit_header_footer():
 
 if authentication_status:
     hide_streamlit_header_footer()
-    col1, col2 = st.columns([3, 5])
+    col1, col2 = st.columns([2, 6])
     with col2:
         st.title("Car Part Identification")
     col1, col2 = st.columns(2)
