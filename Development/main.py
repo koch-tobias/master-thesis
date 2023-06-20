ece9db6efac1d714a93c6d20b28132a073fd097e
koch-tobias <tobi.ko53@googlemail.com>
2023-06-20 14:50:49 +0200

    add function to label new data

--- Development/main.py
+++ Development/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pathlib import Path
 from loguru import logger
 
-from Data_Preprocessing import prepare_and_add_labels, load_csv_into_df
+from Data_Preprocessing import load_csv_into_df
 from LightGBM_Binary import train_lgbm_binary_model
 from LightGBM_Multiclass import train_lgbm_multiclass_model
 from model_predictions import predict_on_new_data
@@ -14,7 +14,7 @@ from Data_Preprocessing import preprocess_dataset
 def main():
     train_lgbm_relevance_model = False
     train_lgbm_name_model = False
-    prediction_on_new_data = False
+    label_new_data = True
     plot_bounding_boxes_one_vehicle = False
     plot_bounding_boxes_all_vehicle_by_name = False
 
@@ -26,18 +26,25 @@ def main():
     if train_lgbm_name_model:
         train_lgbm_multiclass_model()
 
-    if prediction_on_new_data:
-        data_path = Path("../data/original_data_new")
-        dataframes = load_csv_into_df(data_path, original_prisma_data=False, move_to_archive=False)
-        df, ncars = prepare_and_add_labels(dataframes, save_as_excel=False)   
-        df_predicted, einheitsname_not_found, ncars = predict_on_new_data(df[0])
+    if label_new_data:
+        dataframes = load_csv_into_df(original_prisma_data=True, label_new_data=True)
+        for df in dataframes:
+            df_preprocessed, df_relevant_parts, einheitsname_not_found, ncar = predict_on_new_data(df)
 
-        df_predicted.to_excel(f"../data/predicted/{ncars[0]}_relevante_Bauteile.xlsx")
+            for index, row in df_relevant_parts.iterrows():
+                sachnummer = row['Sachnummer']
+                einheitsname = row['Einheitsname']
+                
+                if sachnummer in df['Sachnummer'].values:
+                    df_preprocessed.loc[df_preprocessed['Sachnummer'] == sachnummer, 'Relevant fuer Messung'] = "Ja"
+                    df_preprocessed.loc[df_preprocessed['Sachnummer'] == sachnummer, 'Einheitsname'] = einheitsname
 
-        logger.info("The following car parts are not found in the data:\n",einheitsname_not_found)
-        logger.success(f"The prediction is done and the result is stored here: ../data/predicted/{ncars[0]}_relevante_Bauteile.xlsx!")
+            df_preprocessed.to_excel(f"data/pre_labeled_data/{ncar}_labeled.xlsx")
 
-        logger.info('__________________________________________________________________________________________')
+            logger.info(f"The following car parts are not found in the data: {einheitsname_not_found}")
+            logger.success(f"The prediction is done and the result is stored here: data/pre_labeled_data/{ncar}_labeled.xlsx!")
+
+            logger.info('__________________________________________________________________________________________')
 
     if plot_bounding_boxes_one_vehicle:
         df = pd.read_excel(dataset_path, index_col=0) 
