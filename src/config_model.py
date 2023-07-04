e2de2c655f0b4150f28043267458169db976b59a
koch-tobias <tobi.ko53@googlemail.com>
2023-07-04 16:32:14 +0200

    add colsample_bytree

--- src/config_model.py
+++ src/config_model.py
@@ -1,9 +1,9 @@
 general_params = {
     "seed": 42,
     "cut_percent_of_front": 0.20,
-    "ncars": ['G11', 'G14', 'G15','G20', 'G22', 'G23', 'G29', 'G30', 'G61', 'G65', 'G70','NA5', 'NA7'],
+    "ncars": ['G11', 'G14', 'G15', 'G16', 'G20', 'G21', 'G22', 'G23', 'G26', 'G29', 'G30', 'G31', 'G42', 'G61', 'G65', 'G70','NA5', 'NA7'],
     "relevant_features": ['Sachnummer','Benennung (dt)', 'X-Min','X-Max','Y-Min','Y-Max','Z-Min','Z-Max', 'Wert','Einheit','Gewichtsart','Kurzname','L-Kz.', 'L/R-Kz.', 'Modul (Nr)', 'ox','oy', 'oz', 'xx','xy','xz', 'yx','yy','yz','zx','zy','zz'],
-    "features_for_model": ['center_x', 'center_y', 'center_z','length','width','height','theta_x','theta_y','theta_z'],
+    "features_for_model": ['volume', 'Wert', 'center_x', 'center_y', 'center_z','length','width','height','theta_x','theta_y','theta_z'],
     "keep_modules": ['CE05', 'CD07'], # Keep this modules out of module group EP (CE05=interior mirror, CD07=roof antenna)
     "car_part_designation": "Benennung (dt)"
 }
@@ -27,32 +27,30 @@ model_paths = {
 }
 
 train_settings = {
-    "cross_validation": False,
-    "k-folds": 7,
+    "k-folds": 3,
     "augmentation": True,
     "use_only_text": False,
-    "early_stopping": 30
-}
+    "early_stopping": 50,
+    "test_size": 0.3
+    }
 
 lgbm_params_binary = {
-    "boosting_type": 'dart',
-    "test_size": 0.2,
+    "boosting_type": 'gbdt',
     "metrics": ['auc', 'binary_logloss'],
-     "n_estimators": 300,
+     "n_estimators": 10000,
     "prediction_threshold": 0.75
 }
 
 lgbm_params_multiclass = {
     "boosting_type": 'dart',
-    "test_size": 0.2,
-    "metrics": ['multi_logloss', 'auc_mu'],
-     "n_estimators": 400
+    "metrics": ['auc_mu', 'multi_logloss'],
+     "n_estimators": 500
     }
 
 lgbm_hyperparameter = {
     "lr": [0.05, 0.07, 0.1],    
     "max_depth": [3, 5, 7],
-    "feature_fraction": [0.5, 0.7, 0.9],
+    "colsample_bytree": [0.5, 0.7, 0.9],
     "min_child_samples": [20, 30, 40]
 }
 
