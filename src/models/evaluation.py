4da84a0844d4501da4e687b9720d3ad346adf56a
koch-tobias <tobi.ko53@googlemail.com>
2023-07-04 16:35:47 +0200

    store predictions and trained models

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -1,26 +1,17 @@
 # %%
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
 import pickle
 import os
 
-import lightgbm as lgb
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
-from sklearn.metrics import ConfusionMatrixDisplay
 
-from config_model import general_params, lgbm_params_binary, lgbm_params_multiclass
-from config_model import lgbm_hyperparameter as lgbm_hp
+from config_model import general_params, lgbm_params_binary
 
 # %%
-def store_predictions(model, X_test, y_test, y_pred, probs, folder_path, model_folder_path, binary_model):
-
-    vocabulary_path = model_folder_path + "vocabulary.pkl"
-    # Get the vocabulary of the training data
-    with open(vocabulary_path, 'rb') as f:
-        vocabulary = pickle.load(f)
+def store_predictions(X_test, y_test, y_pred, probs, folder_path, model_folder_path, binary_model):
 
     df_test = pd.read_excel(folder_path + "df_testset.xlsx")
     df_preprocessed = pd.read_excel(folder_path + "df_trainset.xlsx") 
@@ -31,27 +22,7 @@ def store_predictions(model, X_test, y_test, y_pred, probs, folder_path, model_f
         class_names = df_preprocessed["Einheitsname"].unique()
         class_names = sorted(class_names)
 
-    # Extrahieren der wichtigsten Features
-    boost = model.booster_
-    importance = boost.feature_importance()
-    column = boost.feature_name()
-    feature_dict = {vocabulary.shape[0]+index: key for index, key in enumerate(general_params["features_for_model"])}
-
-    if os.path.exists("models/features.xlsx"):
-        pass
-    else:
-        df_features = pd.DataFrame(columns=['Column','Feature','Importance Score'])
-        for j in range(len(column)):
-            df_features.loc[j,"Column"] = column[j]
-            df_features.loc[j,"Importance Score"] = importance[j]
-            if j < vocabulary.shape[0]:
-                df_features.loc[j,"Feature"] = vocabulary[j]
-            else:
-                df_features.loc[j,"Feature"] = feature_dict[j]
-
-        df_features.to_excel("models/features.xlsx")
-
-    df_wrong_predictions = pd.DataFrame(columns=['Benennung (dt)','Predicted','True', 'Probability'])
+    df_wrong_predictions = pd.DataFrame(columns=['Benennung (dt)','Predicted', 'True', 'Probability'])
     # Ausgabe der Vorhersagen, der Wahrscheinlichkeiten und der wichtigsten Features
     for i in range(len(X_test)):
         if y_pred[i] != y_test[i]:
@@ -69,113 +40,83 @@ def store_predictions(model, X_test, y_test, y_pred, probs, folder_path, model_f
     # Serialize data into file:
     df_wrong_predictions.to_excel(model_folder_path + "wrong_predictions.xlsx")
 
+#%%
+def get_features(model, model_folder_path):
+    vocabulary_path = model_folder_path + "vocabulary.pkl"
+    # Get the vocabulary of the training data
+    with open(vocabulary_path, 'rb') as f:
+        vocabulary = pickle.load(f)
+
+    # Extrahieren der wichtigsten Features
+    boost = model.booster_
+    importance = boost.feature_importance()
+    column = boost.feature_name()
+    feature_dict = {vocabulary.shape[0]+index: key for index, key in enumerate(general_params["features_for_model"])}
+
+    if os.path.exists("models/features.xlsx"):
+        df_features = pd.read_excel("models/features.xlsx")
+    else:
+        df_features = pd.DataFrame(columns=['Column','Feature','Importance Score'])
+        for j in range(len(column)):
+            df_features.loc[j,"Column"] = column[j]
+            df_features.loc[j,"Importance Score"] = importance[j]
+            if j < vocabulary.shape[0]:
+                df_features.loc[j,"Feature"] = vocabulary[j]
+            else:
+                df_features.loc[j,"Feature"] = feature_dict[j]
+
+        df_features.to_excel("models/features.xlsx")
+    
+    topx_important_features = df_features.sort_values(by=["Importance Score"], ascending=False).head(20)
+    topx_important_features = topx_important_features.index.tolist()
+    features = df_features["Feature"].values.tolist()
+
+    return features, topx_important_features
 # %%
-def store_trained_model(model, sensitivity, model_folder_path):
+def store_trained_model(model, val_auc, model_folder_path):
     # save model
-    model_path = model_folder_path + f"model_{str(sensitivity)[2:6]}_sensitivity.pkl"
+    if val_auc != -1:
+        model_path = model_folder_path + f"model_{str(val_auc)[2:6]}_validation_auc.pkl"
+    else:
+        model_path = model_folder_path + f"final_model.pkl"
 
     with open(model_path, "wb") as filestore:
         pickle.dump(model, filestore)
 
 # %%
-def evaluate_lgbm_model(model, X_test, y_test, lr_index, max_depth_index, feature_frac_index, child_index, num_models_trained, training_time, binary_model):
-    probs = model.predict_proba(X_test)
+def evaluate_lgbm_model(model, X_test, y_test, evals, lr, max_depth, colsample, child, num_models_trained, training_time, binary_model):
+    probs = model.predict_proba(X_test, num_iteration=model._best_iteration)
     if binary_model:
         y_pred = (probs[:,1] >= lgbm_params_binary["prediction_threshold"])
         y_pred =  np.where(y_pred, 1, 0)
     else:
         y_pred = probs.argmax(axis=1)
 
-    # Print accuracy score
     accuracy = accuracy_score(y_test, y_pred)
     sensitivity = recall_score(y_test, y_pred, average='weighted')
 
-    df_new = pd.DataFrame(columns=["model_name","learningrate","max_depth","num_leaves","feature_freaction","min_child_samples","accuracy", "sensitivity"])
+    df_new = pd.DataFrame(columns=["model_name","learningrate","max_depth","num_leaves","colsample_bytree","min_child_samples", "train auc", "train loss", "validation auc", "validation loss", "test accuracy", "test sensitivity", "Training Time (s)"])
 
-    df_new.loc[num_models_trained, "model_name"] = f"model_{str(sensitivity)[2:6]}"
-    df_new.loc[num_models_trained, "learningrate"] = lgbm_hp["lr"][lr_index]
-    df_new.loc[num_models_trained, "max_depth"] = lgbm_hp["max_depth"][max_depth_index]
-    df_new.loc[num_models_trained, "num_leaves"] = pow(2, lgbm_hp["max_depth"][max_depth_index])
-    df_new.loc[num_models_trained, "feature_freaction"] = lgbm_hp["feature_fraction"][feature_frac_index]
-    df_new.loc[num_models_trained, "min_child_samples"] = lgbm_hp["min_child_samples"][child_index]
-    df_new.loc[num_models_trained, "accuracy"] = accuracy
-    df_new.loc[num_models_trained, "sensitivity"] = sensitivity
-    df_new.loc[num_models_trained, "Training Time (s)"] = training_time
+    if binary_model:
+        val_acc = evals["valid_1"]["auc"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "train auc"] = evals["training"]["auc"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "train loss"] = evals["training"]["binary_logloss"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "validation loss"] = evals["valid_1"]["binary_logloss"][model.best_iteration_-1]
+    else:
+        val_acc = evals["valid_1"]["auc_mu"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "train auc"] = evals["training"]["auc_mu"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "train loss"] = evals["training"]["multi_logloss"][model.best_iteration_-1]
+        df_new.loc[num_models_trained, "validation loss"] = evals["valid_1"]["multi_logloss"][model.best_iteration_-1]
+
+    df_new.loc[num_models_trained, "model_name"] = f"model_{str(val_acc)[2:6]}"
+    df_new.loc[num_models_trained, "learningrate"] = lr
+    df_new.loc[num_models_trained, "max_depth"] = max_depth
+    df_new.loc[num_models_trained, "num_leaves"] = pow(2, max_depth)
+    df_new.loc[num_models_trained, "colsample_bytree"] = colsample
+    df_new.loc[num_models_trained, "min_child_samples"] = child
+    df_new.loc[num_models_trained, "validation auc"] = val_acc
+    df_new.loc[num_models_trained, "test accuracy"] = accuracy
+    df_new.loc[num_models_trained, "test sensitivity"] = sensitivity
+    df_new.loc[num_models_trained, "Training Time (s)"] = training_time 
 
     return y_pred, probs, accuracy, sensitivity, df_new
-
-def store_metrics(model, X_test, y_test, evals, sensitivity, model_folder_path, binary_model):
-
-    probs = model.predict_proba(X_test)
-    if binary_model:
-        y_pred = (probs[:,1] >= lgbm_params_binary["prediction_threshold"])
-        y_pred =  np.where(y_pred, 1, 0)
-    else:
-        y_pred = probs.argmax(axis=1)
-
-    # Print accuracy score
-    accuracy = accuracy_score(y_test, y_pred)
-    print("\n\n Test accuracy:", accuracy)
-    print("Test Sensitivity:", sensitivity, "\n\n")
-
-    if binary_model:
-        plt.rcParams["figure.figsize"] = (10, 10)
-        lgb.plot_metric(evals, metric=lgbm_params_binary["metrics"][1])
-        plt.title("")
-        plt.xlabel('Iterationen', fontsize=12)
-        plt.ylabel('Loss', fontsize=12)
-        plt.xticks(fontsize=12)
-        plt.yticks(fontsize=12)
-        plt.legend(['Training', 'Validation'], fontsize=12)
-        plt.savefig(model_folder_path + 'binary_logloss_plot.png')
-
-        plt.rcParams["figure.figsize"] = (10, 10)
-        lgb.plot_metric(evals, metric=lgbm_params_binary["metrics"][0])
-        plt.title("")
-        plt.xlabel('Iterationen', fontsize=12 )
-        plt.ylabel('AUC', fontsize=12)
-        plt.xticks(fontsize=12)
-        plt.yticks(fontsize=12)
-        plt.legend(['Training', 'Validation'], fontsize=12)
-        plt.savefig(model_folder_path + 'auc_plot.png')
-
-        class_names = ["Not relevant", "Relevant"]
-        plt.rcParams["figure.figsize"] = (15, 15)
-        ConfusionMatrixDisplay.from_predictions(y_test, y_pred, display_labels=class_names, cmap='Blues', colorbar=False,  text_kw={'fontsize': 12})
-        plt.xticks(fontsize=12)
-        plt.yticks(fontsize=12)
-        plt.xlabel('Predicted Label', fontsize=12 )
-        plt.ylabel('True Label', fontsize=12)
-        plt.savefig(model_folder_path + 'confusion_matrix.png')   
-
-    else:    
-        plt.rcParams["figure.figsize"] = (10, 10)
-        lgb.plot_metric(evals, metric=lgbm_params_multiclass["metrics"][0])
-        plt.title("")
-        plt.xlabel('Iterationen', fontsize=12)
-        plt.ylabel('Loss', fontsize=12)
-        plt.xticks(fontsize=12)
-        plt.yticks(fontsize=12)
-        plt.legend(['Training', 'Validation'], fontsize=12)
-        plt.savefig(model_folder_path + 'multi_logloss_plot.png')
-
-        with open(model_folder_path + 'label_encoder.pkl', 'rb') as f:
-            le = pickle.load(f)
-
-        class_names = []
-        classes_pred = le.inverse_transform(y_pred) 
-        classes_true = le.inverse_transform(y_test)
-
-        for name in le.classes_:
-            if (name in classes_pred) or (name in classes_true):
-                class_names.append(name)
-
-        cm_display = ConfusionMatrixDisplay.from_predictions(y_test, y_pred, display_labels=class_names)
-        # Passe die Größe des Diagramms an
-        fig, ax = plt.subplots(figsize=(20, 25))
-        # Zeige die Konfusionsmatrix an
-        cm_display.plot(ax=ax, xticks_rotation='vertical', cmap='Blues', colorbar=False,  text_kw={'fontsize': 12})
-        # Speichere das Diagramm
-        plt.savefig(model_folder_path + 'confusion_matrix.png')
-
-    return accuracy
