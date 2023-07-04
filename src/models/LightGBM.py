b4dfd4560b867f305837b3b6b2b288a2b17d1192
koch-tobias <tobi.ko53@googlemail.com>
2023-07-04 16:31:54 +0200

    add zero_missing and colsample_bytree

--- src/models/LightGBM.py
+++ src/models/LightGBM.py
@@ -3,35 +3,36 @@
 # %%
 from lightgbm import LGBMClassifier
 from config_model import lgbm_params_multiclass, lgbm_params_binary
-from config_model import lgbm_hyperparameter as lgbm_hp
 
 # %%
-def binary_classifier(weight_factor, lr_index, max_depth_index, feature_frac_index, child_index):
+def binary_classifier(weight_factor, lr, max_depth, colsample, child):
     
     class_weight = {0: 1, 1: weight_factor}
     gbm = LGBMClassifier(boosting_type=lgbm_params_binary["boosting_type"],
                         objective='binary',
                         metric=lgbm_params_binary["metrics"],
-                        num_leaves= pow(2, lgbm_hp["max_depth"][max_depth_index]),
-                        max_depth=lgbm_hp["max_depth"][max_depth_index],
-                        learning_rate=lgbm_hp['lr'][lr_index],
-                        feature_fraction=lgbm_hp["feature_fraction"][feature_frac_index],
-                        min_child_samples=lgbm_hp["min_child_samples"][child_index],
+                        num_leaves= pow(2, max_depth),
+                        max_depth=max_depth,
+                        learning_rate=lr,
+                        colsample_bytree=colsample,
+                        min_child_samples=child,
                         n_estimators=lgbm_params_binary["n_estimators"],
+                        zero_as_missing = True,
                         class_weight=class_weight)
     return gbm, lgbm_params_binary["metrics"]
     
-def multiclass_classifier(weight_factor, lr_index, max_depth_index, feature_frac_index, child_index):
+def multiclass_classifier(weight_factor, lr, max_depth, colsample, child):
     gbm = LGBMClassifier(boosting_type=lgbm_params_multiclass["boosting_type"],
                         objective='multiclass',
                         metric=lgbm_params_multiclass["metrics"],
-                        num_leaves= pow(2, lgbm_hp["max_depth"][max_depth_index]),
-                        max_depth=lgbm_hp["max_depth"][max_depth_index],
-                        learning_rate=lgbm_hp['lr'][lr_index],
-                        feature_fraction=lgbm_hp["feature_fraction"][feature_frac_index],
-                        min_child_samples=lgbm_hp["min_child_samples"][child_index],
+                        num_leaves= pow(2, max_depth),
+                        max_depth=max_depth,
+                        learning_rate=lr,
+                        colsample_bytree=colsample,
+                        min_child_samples=child,
                         n_estimators=lgbm_params_multiclass["n_estimators"],
                         num_class=len(weight_factor),
+                        zero_as_missing = True,
                         class_weight=weight_factor
                         )
 
