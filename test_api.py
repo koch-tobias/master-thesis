0e3f020e8737db9499f43250cf17b6fc46de649f
koch-tobias <tobi.ko53@googlemail.com>
2023-07-08 13:57:00 +0200

    move test_api file

--- test_api.py
+++ test_api.py
@@ -2,7 +2,10 @@
 import requests
 import time
 
-url = "http://127.0.0.1:8000/api/get_relevant_parts/" 
+#url = "http://127.0.0.1:8000/api/get_relevant_parts/" 
+
+url = "http://localhost:7070/api/get_relevant_parts/" 
+
 start = time.time()
 file_path = "C:/Users/q617269/Desktop/Masterarbeit_Tobias/repos/master-thesis/data/raw/G20_prismaexport-20230621-143916.xls"
 files = {"file": open(file_path, "rb")}
