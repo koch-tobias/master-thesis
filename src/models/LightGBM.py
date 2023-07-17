57d91b3eb6833e090bb4980c68049aba7aecac15
koch-tobias <tobi.ko53@googlemail.com>
2023-07-17 14:55:05 +0200

    add xgb architecture

--- src/models/LightGBM.py
+++ src/models/LightGBM.py
@@ -2,25 +2,42 @@
 
 # %%
 from lightgbm import LGBMClassifier
-from config_model import lgbm_params_multiclass, lgbm_params_binary
+import xgboost as xgb
+from config_model import lgbm_params_multiclass, lgbm_params_binary, xgb_params_binary, xgb_params_multiclass, train_settings
 
 # %%
-def binary_classifier(weight_factor, lr, max_depth, colsample, child):
+def binary_classifier(weight_factor, hp_dict, method):
     
     class_weight = {0: 1, 1: weight_factor}
-    gbm = LGBMClassifier(boosting_type=lgbm_params_binary["boosting_type"],
-                        objective='binary',
-                        metric=lgbm_params_binary["metrics"],
-                        num_leaves= pow(2, max_depth),
-                        max_depth=max_depth,
-                        learning_rate=lr,
-                        colsample_bytree=colsample,
-                        min_child_samples=child,
-                        n_estimators=lgbm_params_binary["n_estimators"],
-                        class_weight=class_weight)
-    return gbm, lgbm_params_binary["metrics"]
+    if method == "lgbm":
+        model = LGBMClassifier(boosting_type=lgbm_params_binary["boosting_type"],
+                            objective='binary',
+                            metric=lgbm_params_binary["metrics"],
+                            num_leaves= pow(2, hp_dict["max_depth"]),
+                            max_depth=hp_dict["max_depth"],
+                            learning_rate=hp_dict["lr"],
+                            colsample_bytree=hp_dict["colsample_bytree"],
+                            min_child_samples=hp_dict["min_child_samples"],
+                            n_estimators=train_settings["n_estimators"] ,
+                            class_weight=class_weight
+                            )
+        metrics = lgbm_params_binary["metrics"]
+
+    elif method == "xgboost":
+        model = xgb.XGBClassifier(booster=xgb_params_binary["boosting_type"], 
+                            objective="binary:logistic",
+                            eval_metric = xgb_params_binary["metrics"],
+                            max_depth=hp_dict["max_depth"],   
+                            learning_rate=hp_dict["lr"],
+                            colsample_bytree=hp_dict["colsample_bytree"],
+                            scale_pos_weight=weight_factor,
+                            gamma=hp_dict["gamma"],
+                            n_estimators= train_settings["n_estimators"] 
+                        )        
+        metrics = xgb_params_binary["metrics"]
+    return model, metrics
     
-def multiclass_classifier(weight_factor, lr, max_depth, colsample, child):
+def multiclass_classifier(weight_factor, lr, max_depth, colsample, child, method):
     gbm = LGBMClassifier(boosting_type=lgbm_params_multiclass["boosting_type"],
                         objective='multiclass',
                         metric=lgbm_params_multiclass["metrics"],
@@ -29,9 +46,9 @@ def multiclass_classifier(weight_factor, lr, max_depth, colsample, child):
                         learning_rate=lr,
                         colsample_bytree=colsample,
                         min_child_samples=child,
-                        n_estimators=lgbm_params_multiclass["n_estimators"],
+                        n_estimators=train_settings["n_estimators"] ,
                         num_class=len(weight_factor),
                         class_weight=weight_factor
                         )
 
-    return gbm, lgbm_params_multiclass["metrics"]    
+    return gbm, lgbm_params_multiclass["metrics"]     
