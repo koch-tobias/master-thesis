e6fa6462c91e21ec492ef90b543b84342c007928
koch-tobias <tobi.ko53@googlemail.com>
2023-07-25 08:13:17 +0200

    add catboost

--- src/config_model.py
+++ src/config_model.py
@@ -1,72 +1,73 @@
 general_params = {
     "seed": 42,
-    "cut_percent_of_front": 0.18,
-    "relevant_features": ['Sachnummer','Benennung (dt)', 'X-Min','X-Max','Y-Min','Y-Max','Z-Min','Z-Max', 'Wert','Einheit','Gewichtsart','Kurzname','L-Kz.', 'L/R-Kz.', 'Modul (Nr)', 'ox','oy', 'oz', 'xx','xy','xz', 'yx','yy','yz','zx','zy','zz'],
-    "features_for_model": ['volume', 'Wert', 'center_x', 'center_y', 'center_z','length','width','height','theta_x','theta_y','theta_z'],
+    "cut_percent_of_front": 0.18,       # Choose number between 0 and 1. How much percent of the front of the car should be deleted - No relevant car parts in the front of a car
+    "relevant_features": ['Sachnummer','Benennung (dt)', 'X-Min','X-Max','Y-Min','Y-Max','Z-Min','Z-Max', 'Wert','Einheit','Gewichtsart','Kurzname','L-Kz.', 'L/R-Kz.', 'Modul (Nr)', 'ox','oy', 'oz', 'xx','xy','xz', 'yx','yy','yz','zx','zy','zz'], # List of features which are relevant for the models - all other features will be deleted
+    "features_for_model": ['volume', 'Wert', 'center_x', 'center_y', 'center_z','length','width','height','theta_x','theta_y','theta_z'], # List of features which are used for training the models
+    "bounding_box_features_original": ['X-Min', 'X-Max', 'Y-Min', 'Y-Max', 'Z-Min', 'Z-Max', 'ox', 'oy', 'oz', 'xx', 'xy', 'xz', 'yx', 'yy', 'yz', 'zx', 'zy', 'zz', 'Wert'], #List of all original boundingbox features which should be set to 0 if wrong or not given
     "keep_modules": ['CE05', 'CD07'], # Keep this modules out of module group EP (CE05=interior mirror, CD07=roof antenna)
     "car_part_designation": "Benennung (dt)"
 }
 
 paths = {
-    "labeled_data": "data/labeled",
-    "new_data": "data/raw_for_labeling",
-    "model_folder": "models/HyperparameterTuning_07072023_0936"
+    "labeled_data": "data/labeled",                                 # Paths to the labeld datasets
+    "new_data": "data/raw_for_labeling",                            # Paths to the original datasets before labeling
+    "model_folder": "models/HyperparameterTuning_07072023_0936"     # Paths to the directory of the final trained models
 }
 
 gpt_settings = {
-    "temperature": 0.6,
-    "max_tokens": 200,
-    "top_p": 1,
-    "n": 1
+    "temperature": 0.6,            # Choose between 0 and 1. 1: gpt answers strict after guidlines and examples - 0: Fancy answers
+    "max_tokens": 200,             # Maximal number of tokens which are alloud for you request
+    "top_p": 1,                    #
+    "n": 1                         # Number of answers gpt should return
 }
 
 train_settings = {
-    "k-folds": 3,
-    "augmentation": True,
-    "use_only_text": False,
-    "ml-method": "lgbm",
-    "early_stopping": 10,
-    "val_size": 0.3,
-    "test_size": 0.2,
-     "n_estimators": 100
+    "k-folds": 4,                   # Number of folds for k-fold crossvalidation
+    "augmentation": True,           # True for using Data Augmentation 
+    "use_only_text": False,         # True for training on only Designations or False for training on designations and bounding box information
+    "ml-method": "catboost",            # Choose between 'lgbm', 'xgboost' and 'catboost'
+    "early_stopping": 30,           # Number of rounds for early stopping
+    "val_size": 0.4,                # Split dataset into 1-x % training and x % validation
+    "test_size": 0.5,               # Split validation set into 1-x % validation and x % test
+    "top_x_models_for_cv": 0.10,    # Use the top x % of the models after hyperparamter tuning for cross validation
+    "prediction_threshold": 0.75,    # number between 0 and 1. 0.75 means theat the has to be over 75% sure that it is the class to predict it
+     "n_estimators": 10000          # Number of iterations a model will be trained
     }
 
 ###############################
 lgbm_params_binary = {
-    "boosting_type": 'gbdt',
-    "metrics": ['auc', 'binary_logloss'],
-    "prediction_threshold": 0.75
+    "boosting_type": 'gbdt',                # boosting type for lightgbm binary model
+    "metrics": ['auc', 'binary_logloss']    # Area-under-the-curcve and logloss are used for training and evaluation
 }
 
 lgbm_params_multiclass = {
-    "boosting_type": 'gbdt',
-    "metrics": ['auc_mu', 'multi_logloss']
+    "boosting_type": 'gbdt',                # boosting type for lightgbm binary model
+    "metrics": ['auc_mu', 'multi_logloss']  # Area-under-the-curcve mu and logloss are used for training and evaluation
     }
 
 lgbm_hyperparameter = {
-    "lr": [0.05, 0.07, 0.1],    
-    "max_depth": [3, 5, 7],
-    "colsample_bytree": [0.5, 0.7, 0.9],
-    "min_child_samples": [20, 30, 40]
+    "lr": [0.05, 0.07, 0.1],                # Learning rate
+    "max_depth": [3, 5, 7],                 # How long can be the distance from root to leafe node (to reduce overfitting)
+    "colsample_bytree": [0.5, 0.7, 0.9],    # How much percent of the features are used in an iteration (to reduce overfitting)
+    "min_child_samples": [20, 30, 40]       # Minimal number of datapoints a node must contain (to reduce overfitting)
 }
 
 ###############################
 xgb_params_binary = {
-    "boosting_type": 'gbtree',
-    "metrics": ['auc', 'error'],
-    "prediction_threshold": 0.75
+    "boosting_type": 'gbtree',              # Boosting type for Gradient-boosting decision tree
+    "metrics": ['auc', 'error']            # Area-under-the-curve and loss
 }
 
 xgb_params_multiclass = {
-    "boosting_type": 'gbtree',
-    "metrics": ['auc', 'multi:softprob'],
+    "boosting_type": 'gbtree',              # Boosting type for Gradient-boosting decision tree
+    "metrics": ['auc', 'multi:softprob'],   # Area-under-the-curve and multilogloss
     }
 
 xgb_hyperparameter = {
-    "lr": [0.05, 0.1, 0.3],    
-    "max_depth": [4, 6, 9],
-    "colsample_bytree": [0.5, 0.7, 0.9],
-    "gamma": [0, 0.2, 0.5],
+    "lr": [0.05, 0.1, 0.3],                 # Learning rate
+    "max_depth": [4, 6, 9],                 # How long can be the distance from root to leafe node (to reduce overfitting)
+    "colsample_bytree": [0.5, 0.7, 0.9],    # How much percent of the features are used in an iteration (to reduce overfitting)
+    "gamma": [0, 0.2, 0.5],                 # 
 
 }
 '''
@@ -76,6 +77,27 @@ xgb_hyperparameter = {
     "min_child_weight": [4, 6, 9],
 '''
 ###############################
+
+cb_params_binary = {
+    "boosting_type": 'Plain',                # boosting type for lightgbm binary model
+    "metrics": ['AUC', 'CrossEntropy']     # Area-under-the-curcve and crossentropy are used for training and evaluation
+}
+
+cb_params_multiclass = {
+    "boosting_type": 'Plain',                # boosting type for lightgbm binary model
+    "metrics": ['auc_mu', 'multi_logloss']  # Area-under-the-curcve mu and logloss are used for training and evaluation
+    }
+
+cb_hyperparameter = {
+    "lr": [0.05, 0.07, 0.1],                # Learning rate
+    "depth": [4, 6, 9],                     # How long can be the distance from root to leafe node (to reduce overfitting)
+    "colsample_bylevel": [0.5, 0.7, 0.9],   # How much percent of the features are used in an iteration (to reduce overfitting)
+    "min_data_in_leaf": [20, 30, 40]        # Minimal number of datapoints a node must contain (to reduce overfitting)
+}
+
+################################
+
+# Dict which columns should be converted to floats
 convert_dict = {'X-Min': float,
                 'X-Max': float,
                 'Y-Min': float,
