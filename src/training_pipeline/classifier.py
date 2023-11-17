36c8037056160f2e3e934162677c7bf266030636
koch-tobias <tobi.ko53@googlemail.com>
2023-11-17 16:59:41 +0100

    Finalized classifiers

--- src/training_pipeline/classifier.py
+++ src/training_pipeline/classifier.py
@@ -1,15 +1,81 @@
 # %%
 from lightgbm import LGBMClassifier
-import xgboost as xgb
+from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
+
+from sklearn.metrics import fbeta_score
+from sklearn.metrics import make_scorer
+
+import numpy as np
 
 import yaml
 from yaml.loader import SafeLoader
 with open('src/config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
+class CustomFbetaMetric():
+    def get_final_error(self, error, weight):
+        return error
+
+    def is_max_optimal(self):
+        return True
+
+    def evaluate(self, approxes, target, weight):
+
+        y_pred = np.array(approxes).argmax(0)
+            
+        #y_pred = [np.argmax(x) for x in approxes]
+        y_true = np.array(target)
+
+        # Calculate the F-beta score for multiclass classification
+        fbeta = fbeta_score(y_true, y_pred, beta=2, average='weighted')
+
+        # Return the fbeta score
+        return fbeta, 0
+
 class Classifier:
 
+    @staticmethod
+    def costum_fbeta_score(y_true, y_pred):
+        
+        # Define the beta value
+        beta = 2
+
+        y_pred = np.round(y_pred)
+        # Calculate the F-beta score with the specified beta value
+        fbeta = fbeta_score(y_true, y_pred, beta=beta)
+
+        return 'fbeta', fbeta, True
+
+    @staticmethod
+    def costum_fbeta_score_multi(y_true, y_pred):
+        
+        # Define the beta value
+        beta = 2
+        # Round the predicted probabilities to get the predicted labels
+        y_pred_labels = np.argmax(y_pred, axis=1)
+        
+        # Calculate the F-beta score with the specified beta value
+        fbeta = fbeta_score(y_true, y_pred_labels, beta=beta, average='weighted')
+        
+        return 'fbeta', fbeta, True
+    
+    def xgb_custom_fbeta_score(preds, dtrain):
+        beta = 2  # Define the desired value for beta
+        #labels = dtrain.get_label()  # Get the true labels from dtrain
+        classes = np.repeat(0, dtrain.shape[0])
+        classes[dtrain > 0.5] = 1
+        y_pred = classes
+
+        # Convert probability outputs to binary predictions
+        preds_binary = np.round(preds)
+
+        # Compute the F-beta score using sklearn's fbeta_score function
+        fbeta = fbeta_score(y_pred, preds_binary, beta=beta)
+        
+        return fbeta
+
+        
     @staticmethod
     def binary_classifier(weight_factor: int, hp: dict, method: str):
         '''
@@ -23,50 +89,51 @@ class Classifier:
             metrics: a list of strings representing the performance metrics to be used for training the model.
         '''
         
-        class_weight = {0: 1, 1: weight_factor}
+        class_weight = {0: weight_factor, 1: 1}
+
         if method == "lgbm":
             model = LGBMClassifier(boosting_type=config["lgbm_params_binary"]["boosting_type"],
                                     objective='binary',
-                                    metric=config["lgbm_params_binary"]["metrics"],
-                                    num_leaves= pow(2, hp["max_depth"]),
+                                    metric='None',
                                     max_depth=hp["max_depth"],
                                     learning_rate=hp["lr"],
                                     colsample_bytree=hp["colsample_bytree"],
-                                    min_child_samples=hp["min_child_samples"],
+                                    reg_lambda=hp["lambda_l2"],
                                     n_estimators=config["train_settings"]["n_estimators"] ,
-                                    class_weight=class_weight
+                                    class_weight=class_weight,
+                                    random_state=42
                                 )
-            metrics = config["lgbm_params_binary"]["metrics"]
+            metrics = [config["lgbm_params_binary"]["loss"], Classifier.costum_fbeta_score]
 
         elif method == "xgboost":
-            model = xgb.XGBClassifier(booster=config["xgb_params_binary"]["boosting_type"], 
+            metrics =  Classifier.xgb_custom_fbeta_score
+
+            model = XGBClassifier(booster=config["xgb_params_binary"]["boosting_type"], 
                                         objective="binary:logistic",
-                                        eval_metric = config["xgb_params_binary"]["metrics"],
+                                        eval_metric = metrics,
                                         max_depth=hp["max_depth"],   
                                         learning_rate=hp["lr"],
                                         colsample_bytree=hp["colsample_bytree"],
+                                        reg_lambda=hp["lambda"],
                                         scale_pos_weight=weight_factor,
-                                        gamma=hp["gamma"],
                                         n_estimators= config["train_settings"]["n_estimators"],
-                                        #tree_method= 'gpu_hist',
-                                        #predictor='gpu_predictor',
-                                        #gpu_id=5,
-                                        verbosity=0
+                                        verbosity=0,
+                                        random_state=42
                                     )        
-            metrics = config["xgb_params_binary"]["metrics"]
 
         elif method == "catboost":
             model = CatBoostClassifier(iterations=config["train_settings"]["n_estimators"], 
                                         learning_rate=hp["lr"], 
                                         depth=hp["depth"],
-                                        colsample_bylevel=hp["colsample_bylevel"],                          # Not supported by GPU
-                                        min_data_in_leaf=hp["min_data_in_leaf"],
-                                        loss_function=config["cb_params_binary"]["metrics"][1],
-                                        eval_metric=config["cb_params_binary"]["metrics"][0],
+                                        l2_leaf_reg=hp["l2_regularization"],
+                                        bagging_temperature=hp["bagging_temperature"],        
+                                        loss_function=config["cb_params_binary"]["loss"],
+                                        eval_metric='F:beta=2',
                                         early_stopping_rounds=config["train_settings"]["early_stopping"],
-                                        use_best_model=True
+                                        use_best_model=True,
+                                        random_seed=42
                                     )
-            metrics = config["cb_params_binary"]["metrics"]
+            metrics = [config["cb_params_binary"]["loss"], 'F:beta=2']
             
         return model, metrics
         
@@ -83,48 +150,48 @@ class Classifier:
             metrics: a list of strings representing the performance metrics to be used for training the model.
         '''
         if method == 'lgbm':
+            metrics = config["lgbm_params_multiclass"]["loss"]
             model = LGBMClassifier(boosting_type=config["lgbm_params_multiclass"]["boosting_type"],
                                     objective='multiclass',
-                                    metric=config["lgbm_params_multiclass"]["metrics"],
-                                    num_leaves= pow(2, hp["max_depth"]),
+                                    metric=metrics,
                                     max_depth=hp["max_depth"],
                                     learning_rate=hp["lr"],
                                     colsample_bytree=hp["colsample_bytree"],
-                                    min_child_samples=hp["min_child_samples"],
+                                    reg_lambda=hp["lambda_l2"],
+                                    min_child_samples=20,
                                     n_estimators=config["train_settings"]["n_estimators"] ,
                                     num_class=len(weight_factor),
-                                    class_weight=weight_factor
+                                    class_weight=weight_factor,
+                                    random_state=42
                                 )
-            metrics = config["lgbm_params_multiclass"]["metrics"]
             
         elif method == "xgboost":
-            model = xgb.XGBClassifier(booster=config["xgb_params_multiclass"]["boosting_type"], 
+            metrics = config["xgb_params_multiclass"]["loss"]
+            model = XGBClassifier(booster=config["xgb_params_multiclass"]["boosting_type"], 
                                         objective="multi:softprob",
-                                        eval_metric = config["xgb_params_multiclass"]["metrics"],
+                                        eval_metric = metrics,
                                         max_depth=hp["max_depth"],   
                                         learning_rate=hp["lr"],
                                         colsample_bytree=hp["colsample_bytree"],
                                         scale_pos_weight=weight_factor,
-                                        gamma=hp["gamma"],
+                                        reg_lambda=hp["lambda"],
                                         n_estimators= config["train_settings"]["n_estimators"],
-                                        #tree_method= 'gpu_hist',
-                                        #predictor='gpu_predictor',
-                                        #gpu_id=5,
-                                        verbosity=0
+                                        verbosity=0,
+                                        random_state=42
                                     )   
-            metrics = config["xgb_params_multiclass"]["metrics"]
         
         elif method == "catboost":
+            metrics = config["cb_params_multiclass"]["loss"]
             model = CatBoostClassifier(iterations=config["train_settings"]["n_estimators"], 
                                         learning_rate=hp["lr"], 
                                         depth=hp["depth"],
-                                        colsample_bylevel=hp["colsample_bylevel"],                      # Not supported by GPU
-                                        min_data_in_leaf=hp["min_data_in_leaf"],
-                                        loss_function=config["cb_params_multiclass"]["metrics"][1],
-                                        eval_metric=config["cb_params_multiclass"]["metrics"][0],
+                                        l2_leaf_reg=hp["l2_regularization"],
+                                        bagging_temperature=hp["bagging_temperature"],
+                                        loss_function=config["cb_params_multiclass"]["loss"],
+                                        eval_metric=config["cb_params_multiclass"]["loss"],
                                         early_stopping_rounds=config["train_settings"]["early_stopping"],
-                                        use_best_model=True
+                                        use_best_model=True,
+                                        random_seed=42
                                     )
-            metrics = config["cb_params_multiclass"]["metrics"]
 
         return model, metrics
