19a46dcafbb666c0b73a21d09b881697e816f0cd
koch-tobias <tobi.ko53@googlemail.com>
2023-11-30 14:30:07 +0100

    finalize train

--- src/training_pipeline/train.py
+++ src/training_pipeline/train.py
@@ -142,7 +142,7 @@ def fit_eval_model(X_train: np.array, y_train: np.array, X_val: np.array, y_val:
     stop = time.time()
     training_time = int(stop - start)
 
-    train_loss, fbeta_train, val_loss, precision_val, sensitivity_val, fbeta_val, sensitivity_test, precision_test, fbeta_test, df_new = evaluate_model(gbm, X_test, y_test, X_val, y_val, X_train, y_train, evals, hp_in_iteration, num_models_trained, training_time, df.columns, binary_model=binary_model, method=method)    
+    train_loss, fbeta_train, val_loss, precision_val, sensitivity_val, fbeta_val, loss_test, sensitivity_test, precision_test, fbeta_test, df_new = evaluate_model(gbm, X_test, y_test, X_val, y_val, X_train, y_train, evals, hp_in_iteration, num_models_trained, training_time, df.columns, binary_model=binary_model, method=method)    
     best_iteration = Identifier.get_best_iteration(gbm, method) 
     
     df = pd.concat([df, df_new])
@@ -155,6 +155,7 @@ def fit_eval_model(X_train: np.array, y_train: np.array, X_val: np.array, y_val:
     model_results_dict["val_sensitivity"].append(sensitivity_val)
     model_results_dict["val_precision"].append(precision_val)
     model_results_dict["val_fbeta_score"].append(fbeta_val)
+    model_results_dict["test_loss"].append(loss_test)
     model_results_dict["test_sensitivity"].append(sensitivity_test)
     model_results_dict["test_precision"].append(precision_test)
     model_results_dict["test_fbeta_score"].append(fbeta_test)
@@ -175,7 +176,7 @@ def create_result_df(hp_dict: dict) -> tuple[pd.DataFrame, int]:
         df (Pandas dataframe): A dataframe to store the results of the model training process.
         total_models (int): An integer representing the total number of models that will be trained using this dataframe.
     '''
-    fix_columns = ["model_name", "train loss", "train fbeta_score", "validation loss", "validation precision", "validation sensitivity", "validation fbeta_score", "test sensitivity", "test precision", "test fbeta_score", "Training Time (s)", "patience", "Trained iterations"]
+    fix_columns = ["model_name", "train loss", "train fbeta_score", "validation loss", "validation precision", "validation sensitivity", "validation fbeta_score", "test loss", "test sensitivity", "test precision", "test fbeta_score", "Training Time (s)", "patience", "Trained iterations"]
 
     total_models = 1
     for hp in hp_dict:
@@ -186,7 +187,7 @@ def create_result_df(hp_dict: dict) -> tuple[pd.DataFrame, int]:
 
     return df, total_models
 
-def grid_search(X_train: np.array, y_train: np.array, X_val: np.array, y_val: np.array, X_test: np.array, y_test: np.array, weight_factor: float or dict, hp_dict: dict, binary_model: bool, method: str) -> tuple[pd.DataFrame, dict, dict]:
+def grid_search(X_train: np.array, y_train: np.array, X_val: np.array, y_val: np.array, X_test: np.array, y_test: np.array, weight_factor: float or dict, hp_dict: dict, unique_names:list, binary_model: bool, method: str) -> tuple[pd.DataFrame, dict, dict]:
     ''' 
     This function performs model training with grid search hyperparameter tuning. It takes training, validation, and testing data as input, along with a dictionary of hyperparameters to try, as well as a boolean value indicating whether the model is binary or multiclass, and a method used for feature selection. It returns a dataframe containing the results of the model training process, as well as a dictionary of different evaluation metrics for each trained model.
     Args:
@@ -208,7 +209,7 @@ def grid_search(X_train: np.array, y_train: np.array, X_val: np.array, y_val: np
 
     logger.info("Start training with grid search hyperparameter tuning...")
 
-    model_results_dict = {"models_list": [], "evals_list": [], "train_loss": [], "train_fbeta_score": [], "val_loss": [], "val_sensitivity": [], "val_precision": [], "val_fbeta_score": [], "test_sensitivity": [], "test_precision": [], "test_fbeta_score": [], "best_iteration": []}
+    model_results_dict = {"models_list": [], "evals_list": [], "train_loss": [], "train_fbeta_score": [], "val_loss": [], "val_sensitivity": [], "val_precision": [], "val_fbeta_score": [], "test_loss": [], "test_sensitivity": [], "test_precision": [], "test_fbeta_score": [], "best_iteration": []}
     num_models_trained = 0
 
     df, total_models = create_result_df(hp_dict)
@@ -233,7 +234,8 @@ def get_fold_data(X: np.array, y: np.array, train_index: int, val_index: int) ->
     Args:
         X: The feature data.
         y: The label data.
-        train_index: The indices of the training data for the current fold.
+        train_index: The indices of the training data for the current fold.979991
+
         val_index: The indices of the validation data for the current fold.
     Return:
         X_train_fold: The feature data for the training set for the current fold.
@@ -248,7 +250,7 @@ def get_fold_data(X: np.array, y: np.array, train_index: int, val_index: int) ->
     return X_train_fold, X_val_fold, y_train_fold, y_val_fold
 
 
-def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: np.array, weight_factor: float or dict, df: pd.DataFrame, model_results_dict: dict, folder_path: Path, method: str, binary_model: bool) -> pd.DataFrame:
+def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: np.array, weight_factor: float or dict, df: pd.DataFrame, method: str, binary_model: bool) -> pd.DataFrame:
     ''' 
     This function performs k-fold cross-validation for the top x models generated by grid search hyperparameter tuning. 
     It takes training, validation, and testing data as input, along with a weight factor for the loss function, a dataframe containing the results of the grid search hyperparameter tuning, a dictionary of different evaluation metrics for each trained model, the folder path to store the models and results, the method used for feature selection, and a boolean value indicating whether the model is binary or multiclass. 
@@ -269,7 +271,7 @@ def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: n
     '''
     kfold = StratifiedKFold(n_splits=config["train_settings"]["k-folds"], shuffle=True, random_state=config["dataset_params"]["seed"])
 
-    fix_columns = ["model_name", "avg train loss","avg train fbeta_score", "avg validation loss",  "avg validation sensitivity", "avg validation precision", "avg validation fbeta_score", "avg test sensitivity", "avg test precision", "avg test fbeta_score"]
+    fix_columns = ["model_name", "avg train loss","avg train fbeta_score", "avg validation loss",  "avg validation sensitivity", "avg validation precision", "avg validation fbeta_score", "avg test loss", "avg test sensitivity", "avg test precision", "avg test fbeta_score"]
 
     if method == "lgbm":
         hp_columns = list(config["lgbm_hyperparameter"].keys())
@@ -282,13 +284,17 @@ def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: n
     df_cv = pd.DataFrame(columns=columns)
 
     top_x_models = int(round(df.shape[0] * config["train_settings"]["top_x_models_for_cv"]))
+
+    if top_x_models == 0:
+        top_x_models = 1
+
     number_of_folds = config["train_settings"]["k-folds"]
     total_cv_models = number_of_folds * top_x_models
     count_trained_models = 0
 
     logger.info(f"Start to validate the top {top_x_models} models by using {number_of_folds}-fold cross-validation... ")
     for i in range(top_x_models):
-        cv_results_dict = {"models_list": [], "evals_list": [], "train_loss": [], "train_fbeta_score": [], "val_loss": [], "val_sensitivity": [],  "val_precision": [],  "val_fbeta_score": [], "test_sensitivity": [],  "test_precision": [],  "test_fbeta_score": [], "best_iteration": []}
+        cv_results_dict = {"models_list": [], "evals_list": [], "train_loss": [], "train_fbeta_score": [], "val_loss": [], "val_sensitivity": [],  "val_precision": [],  "val_fbeta_score": [],"test_loss": [], "test_sensitivity": [],  "test_precision": [],  "test_fbeta_score": [], "best_iteration": []}
         logger.info(f"Start {number_of_folds}-fold cross-validation for model {i+1}:")
         for train_index, val_index in kfold.split(X, y):
             
@@ -307,6 +313,7 @@ def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: n
         avg_val_sensitivity = round(mean(cv_results_dict["val_sensitivity"]), 6)
         avg_val_fbeta = round(mean(cv_results_dict["val_fbeta_score"]), 6)
 
+        avg_test_loss = round(mean(cv_results_dict["test_loss"]), 6)
         avg_test_precision = round(mean(cv_results_dict["test_precision"]), 6)
         avg_test_sensitivity = round(mean(cv_results_dict["test_sensitivity"]), 6)
         avg_test_fbeta = round(mean(cv_results_dict["test_fbeta_score"]), 6)
@@ -321,6 +328,7 @@ def k_fold_crossvalidation(X: np.array, y: np.array, X_test: np.array, y_test: n
         df_cv.loc[i, "avg validation sensitivity"] = avg_val_sensitivity
         df_cv.loc[i, "avg validation precision"] = avg_val_precision
         df_cv.loc[i, "avg validation fbeta_score"] = avg_val_fbeta
+        df_cv.loc[i, "avg test loss"] = avg_test_loss
         df_cv.loc[i, "avg test sensitivity"] = avg_test_sensitivity
         df_cv.loc[i, "avg test precision"] = avg_test_precision
         df_cv.loc[i, "avg test fbeta_score"] = avg_test_fbeta
@@ -361,39 +369,41 @@ def train_model(folder_path: Path, binary_model: bool, method: str):
     # Training with the hold out method and grid search hyperparameter tuning
     data_folder = Path(config["train_settings"]["folder_processed_dataset"])
     X_train, y_train, X_val, y_val, X_test, y_test, df_preprocessed, df_train, df_val, df_test, weight_factor = load_training_data(data_folder, binary_model=binary_model)
-
+    
     # Copy the label encoder from the dataset path to the model folder. This file is used in the deployment process.
     copy_labelencoder(model_folder_path)
 
-    # Store the list of uniform names (for the xAi pipeline)
+    # Store the list of uniform names (for the xAi pipeline) 
     unique_names = df_preprocessed[config['labels']['multiclass_column']].unique().tolist()
     with open(os.path.join(model_folder_path, "list_of_uniform_names.pkl"), 'wb') as f:
         pickle.dump(unique_names, f)
 
     # Start grid search hyperparamter tuning
-    df, model_results_dict, metrics = grid_search(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test, weight_factor=weight_factor, hp_dict=hp_dict, binary_model=binary_model, method=method)
+    df, model_results_dict, metrics = grid_search(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, X_test=X_test, y_test=y_test, weight_factor=weight_factor, hp_dict=hp_dict, unique_names=unique_names, binary_model=binary_model, method=method)
 
-    df.to_csv(os.path.join(model_folder_path, "hyperparametertuning_results.csv"))
+    with open(os.path.join(model_folder_path, "model_results_dict.pkl"), 'wb') as f:
+        pickle.dump(model_results_dict, f)    
 
     # K-Fold Cross-Validation with the 5 best model according to the validation auc after grid search 
-    df = df.sort_values(by=["validation fbeta_score"], ascending=False)
+    df = df.sort_values(by=["validation fbeta_score", "validation loss"], ascending=[False, True])
+
+    df.to_csv(os.path.join(model_folder_path, "hyperparametertuning_results.csv"))
 
     X = np.concatenate((X_train, X_val), axis=0)
     y = np.concatenate((y_train, y_val), axis=0)
 
-    df_cv = k_fold_crossvalidation(X=X, y=y, X_test=X_test, y_test=y_test, weight_factor=weight_factor, df=df, model_results_dict=model_results_dict, folder_path=folder_path, method=method, binary_model=binary_model)
+    df_cv = k_fold_crossvalidation(X=X, y=y, X_test=X_test, y_test=y_test, weight_factor=weight_factor, df=df, method=method, binary_model=binary_model)
+
+    df_cv = df_cv.sort_values(by=["avg validation fbeta_score", "avg validation loss"], ascending=[False, True])
 
     df_cv.to_csv(os.path.join(model_folder_path, "crossvalidation_results.csv"))
-    df_cv = df_cv.sort_values(by=["avg validation fbeta_score"], ascending=False)
 
     index_best_model = int(df_cv["index"].iloc[0])
+    print(index_best_model)
     logger.info(model_results_dict["val_fbeta_score"][index_best_model])
-    logger.info("Start storing the best model with metrics plots and wrong predictions according to the validation auc...")
+    logger.info("Start storing the best model with metrics plots and wrong predictions according to the validation f2-score...")
     best_model = model_results_dict["models_list"][index_best_model]
     best_iteration = model_results_dict["best_iteration"][index_best_model]
-
-    with open(os.path.join(model_folder_path, "model_results_dict.pkl"), 'wb') as f:
-        pickle.dump(model_results_dict, f)    
 
     if method == 'lgbm':
         Visualization.store_metrics(evals=model_results_dict["evals_list"][index_best_model], best_iteration=best_iteration, model_folder_path=model_folder_path, binary_model=binary_model, finalmodel=False)
@@ -403,26 +413,27 @@ def train_model(folder_path: Path, binary_model: bool, method: str):
     hp_keys = list(hp_dict.keys())
     best_hp = {}
     for key in hp_keys:
-        best_hp[key] = df[key].iloc[index_best_model]
+        best_hp[key] = df_cv[key].iloc[0]
 
-    store_trained_model(model=best_model, metrics=metrics, best_iteration=best_iteration, val_fbeta=df_cv["avg validation fbeta_score"].iloc[0], hp=best_hp, index_best_model=index_best_model, model_folder_path=model_folder_path, finalmodel=False)
+    store_trained_model(model=best_model, metrics=metrics, best_iteration=best_iteration, val_fbeta=df_cv["avg validation fbeta_score"].iloc[0], val_sensitivity=df_cv["avg validation sensitivity"].iloc[0], val_precision=df_cv["avg validation precision"].iloc[0], hp=best_hp, index_best_model=index_best_model, model_folder_path=model_folder_path, finalmodel=False)
     y_pred, probs, best_iteration = Identifier.model_predict(model=best_model, X_test=X_test, method=method, binary_model=binary_model)
     Visualization.store_confusion_matrix(y_test=y_test, y_pred=y_pred, model_folder_path=model_folder_path, binary_model=binary_model)
     store_predictions(y_test=y_test, y_pred=y_pred, probs=probs, df_preprocessed=df_preprocessed, df_test=df_test, model_folder_path=model_folder_path, binary_model=binary_model)
     logger.success("Storing was successfull!")
-
+    
     logger.info("Train a new model on the entire data with the parameters of the best identified model...")
+
     X_train = np.concatenate((X_train, X_test), axis=0)
     y_train = np.concatenate((y_train, y_test), axis=0)
 
     gbm_final, evals_final, metrics = model_fit(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, weight_factor=weight_factor, hp_in_iteration=best_hp, binary_model=binary_model, method=method)
     best_iteration = Identifier.get_best_iteration(model=gbm_final, method=method)
 
-    y_pred_val, probs, _ = Identifier.model_predict(model=gbm_final, X_test=X_val, method=method, binary_model=binary_model)
+    y_pred_val, probs_val, _ = Identifier.model_predict(model=gbm_final, X_test=X_val, method=method, binary_model=binary_model)
 
-    precision_val, sensitivity_val, fbeta_val = calculate_metrics(y_val, y_pred_val, binary_model)
+    precision_val, sensitivity_val, fbeta_val, loss_val = calculate_metrics(y_val, y_pred_val, binary_model, probs_val)
 
-    store_trained_model(model=gbm_final, metrics=metrics, best_iteration=best_iteration, val_fbeta=fbeta_val, hp=best_hp, index_best_model=index_best_model, model_folder_path=model_folder_path, finalmodel=True)
+    store_trained_model(model=gbm_final, metrics=metrics, best_iteration=best_iteration, val_fbeta=fbeta_val, val_sensitivity=sensitivity_val, val_precision=precision_val, hp=best_hp, index_best_model=index_best_model, model_folder_path=model_folder_path, finalmodel=True)
 
     if method == 'lgbm':
         Visualization.store_metrics(evals=evals_final, best_iteration=best_iteration, model_folder_path=model_folder_path, binary_model=binary_model, finalmodel=True)
